f15c1e4d0c38c065d7a97112da69b5b5679fc4fb
Martin <mgrao@ikerlan.es>
2026-01-07 14:46:26 +0100

    More changes

--- setup_launch/launch/setup_launch.launch.py
+++ setup_launch/launch/setup_launch.launch.py
@@ -1,5 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
@@ -32,59 +32,61 @@ def generate_launch_description():
         ],
         output='screen'
     )
+    
 
     # Launch del driver del UR
     ur_control_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution(
-                [
-                    FindPackageShare("ur_robot_driver"),
-                    "launch",
-                    "ur_control.launch.py",
-                ]
+                PythonLaunchDescriptionSource(
+                    PathJoinSubstitution(
+                        [
+                            FindPackageShare("setup_launch"),
+                            "launch",
+                            "ur_control.launch.py",
+                        ]
+                    )
+                ),
+                launch_arguments={
+                    "ur_type": "ur10e",
+                    "robot_ip": robot_ip,
+                    "launch_rviz": "false",
+                    "use_fake_hardware": "false",
+                    "initial_joint_controller": "scaled_joint_trajectory_controller",
+                    "activate_joint_controller": "true",
+                    "description_package": "setup_description",
+                    "description_file": "robot.urdf.xacro",
+                    "use_tool_communication": "true",
+                    "gripper_com_port": "/tmp/ttyUR"
+                }.items(),
             )
-        ),
-        launch_arguments={
-            "ur_type": "ur10e",
-            "robot_ip": robot_ip,
-            "launch_rviz": "false",
-            "use_fake_hardware": "false",
-            "initial_joint_controller": "scaled_joint_trajectory_controller",
-            "activate_joint_controller": "true",
-            "description_package": "setup_description",
-            "description_file": "robot.urdf.xacro",
-            "use_tool_communication": "true"
-        }.items(),
-    )
     
     # Launch de MoveIt
     moveit_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution(
-                [
-                    FindPackageShare("setup_moveit_config"),
-                    "launch",
-                    "ur_moveit.launch.py",
-                ]
+                PythonLaunchDescriptionSource(
+                    PathJoinSubstitution(
+                        [
+                            FindPackageShare("setup_moveit_config"),
+                            "launch",
+                            "ur_moveit.launch.py",
+                        ]
+                    )
+                ),
+                launch_arguments={
+                    "ur_type": "ur10e",
+                    "use_rviz": use_rviz,
+                    "launch_rviz": use_rviz,
+                    "robot_ip": robot_ip,
+                    "description_package": "setup_description",
+                    "description_file": "robot.urdf.xacro",
+                    "moveit_config_package": "setup_moveit_config",
+                    "moveit_config_file": "ur.srdf.xacro",
+                    "gripper_com_port": "/tmp/ttyUR"
+                }.items(),
             )
-        ),
-        launch_arguments={
-            "ur_type": "ur10e",
-            "use_rviz": use_rviz,
-            "launch_rviz": use_rviz,
-            "robot_ip": robot_ip,
-            "description_package": "setup_description",
-            "description_file": "robot.urdf.xacro",
-            "moveit_config_package": "setup_moveit_config",
-            "moveit_config_file": "ur.srdf.xacro",
-            "gripper_com_port": "/tmp/ttyUR"
-        }.items(),
-    )
 
     return LaunchDescription(
         declared_arguments +
         [
-            socat_node,
+            # socat_node,
             ur_control_launch,
             moveit_launch,
         ]
